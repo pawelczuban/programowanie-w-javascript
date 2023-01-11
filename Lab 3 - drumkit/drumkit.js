4dd2b5d28f144932f6fb226d52ad0237156cb7d6
pawelczuban <80459075+pawelczuban@users.noreply.github.com>
2023-01-11 16:46:39 +0100

    fix

--- Lab 3 - drumkit/drumkit.js
+++ Lab 3 - drumkit/drumkit.js
@@ -1,22 +1,11 @@
 // notatnik 
 document.addEventListener('keypress', onKeyPress)
 
-const KeyToSound = {
-    'a': 's1',
-    's': 's2',
-    'd': 's3',
-    'e': 's4',
-    'f': 's5',
-    'g': 's6',
-    'h': 's7',
-    'j': 's8',
-    'k': 's9',
-}
-function onKeyPress(ev) {
-    // const key = event.key
+function onKeyPress(event) {
+    const key = event.key
     // logika mapowania key -> sound
-    const sound = KeyToSound[ev.key]
-    // switch (ev.key) {
+    //const sound = KeyToSound[event.key]
+    // switch (event.key) {
     //     case 'a':
     //         sound = SOUND.clap
     //         // clap
@@ -26,14 +15,46 @@ function onKeyPress(ev) {
     //         // hihat
     //         break;
     // }
-    playSound(sound)
+    //playSound(sound)
+
+    document.getElementById("message").innerHTML = "The pressed key was: " + key;
+
+    switch(key) {
+        case 'b':
+            playSound('boom')
+            break;
+        case 'c':
+            playSound('clap')
+            break;
+        case 'h':
+            playSound('hihat')
+            break;
+        case 'k':
+            playSound('kick')
+            break;
+        case 'o':
+            playSound('openhat')
+            break;
+        case 'r':
+            playSound('ride')
+            break;
+        case 's':
+            playSound('snare')
+            break;
+        case 't':
+            playSound('tink')
+            break;
+        case 'x':
+            playSound('tom')
+            break;
+    }
 }
 
 function playSound(sound) {
-    if (!sound) {
-        return
-    }
-    const audioTag = document.querySelector(`#${clap}`)
+    // if (!sound) {
+    //     return
+    // }
+    const audioTag = document.querySelector(`#` + sound)
     audioTag.currentTime = 0
     audioTag.play()
 }
